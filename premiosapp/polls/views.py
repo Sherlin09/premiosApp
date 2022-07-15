fd903a87d734a5b03edfc20f11804e0e40204fe0
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-07-14 19:31:09 -0500

    creacion de vistas

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("estas en la pagina principal de premios Platzi")
+
+def detail(request, question_id):
+    return HttpResponse(f"estás viendo la pregunta número {question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"estas iendo los resultados de la pregunta numero {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta número {question_id}")
+
