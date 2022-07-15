8046a5eb02db886083cdc36bcca64bb41c75353c
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-07-14 20:19:17 -0500

    creacion de template de home

--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 def index(request):
-    return HttpResponse("estas en la pagina principal de premios Platzi")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
 
 def detail(request, question_id):
     return HttpResponse(f"estás viendo la pregunta número {question_id}")
